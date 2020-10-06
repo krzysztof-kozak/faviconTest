ac309f02e717e8e10ea6208772422ed86487fbff
Jan Nowak <gorskimarcin8@gmail.com>
2020-10-07 01:42:22 +0200

    base for js, wo localstorage and wo animations

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,28 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+let counter = document.querySelector(".hydrapp__counter")
+const addGlass = document.querySelector(".button__addGlass")
+const removeGlass = document.querySelector(".button__removeGlass")
+
+let counterValue = '0';
+
+addGlass.addEventListener('click', () => {
+    counterValue++;
+    counter.innerHTML = counterValue;
+    console.log('???');
+} )
+
+removeGlass.addEventListener('click', () => {
+    if(counterValue >= 1) {
+    counterValue--;
+    counter.innerHTML = counterValue;
+    console.log('!!!');
+}else{
+    //do nothing;
+}
+})
+
+//Wartość szklanek przechowuj w localstorage pod kluczem z
+// dzisiejszą datą. const key = new Date().toISOString().slice(0, 10)
