fd1f322058bd050df015c6b554dfda791587ce94
Jan Nowak <gorskimarcin8@gmail.com>
2020-10-19 05:23:33 +0200

    Hydrapp 1.0.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,34 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import { registerSW } from "./pwa.js";
+registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
 
-let counter = document.querySelector(".hydrapp__counter")
-const addGlass = document.querySelector(".button__addGlass")
-const removeGlass = document.querySelector(".button__removeGlass")
+let counter = document.querySelector(".hydrapp__counter");
+const addGlass = document.querySelector(".button__addGlass");
+const removeGlass = document.querySelector(".button__removeGlass");
 
-let counterValue = '0';
+let counterValue = "0";
 
-addGlass.addEventListener('click', () => {
-    counterValue++;
-    counter.innerHTML = counterValue;
-    console.log('???');
-} )
+addGlass.addEventListener("click", () => {
+  counterValue++;
+  counter.innerHTML = counterValue;
+  console.log("???");
+});
 
-removeGlass.addEventListener('click', () => {
-    if(counterValue >= 1) {
+removeGlass.addEventListener("click", () => {
+  if (counterValue >= 1) {
     counterValue--;
     counter.innerHTML = counterValue;
-    console.log('!!!');
-}else{
+    console.log("!!!");
+  } else {
     //do nothing;
-}
-})
+  }
+});
 
 //Wartość szklanek przechowuj w localstorage pod kluczem z
 // dzisiejszą datą. const key = new Date().toISOString().slice(0, 10)
